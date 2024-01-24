5737bdc06f7c98a53096c7cf97a64bfbfb8e78ab
clairemuiru1 <muiruclaire@gmail.com>
2024-01-24 15:53:17 +0300

    "Done with Get"

--- server/app.py
+++ server/app.py
@@ -35,16 +35,111 @@ class Logout(Resource):
     pass
 
 class GarageResource(Resource):
-    pass
+    def get(self):
+        garages = []
+        for garage in Garage.query.all():
+            garage_dict = {
+                "id": garage.id,
+                "name" : garage.name,
+                "location": garage.location,
+                "contact_number": garage.contact_number,
+            }
+            garages.append(garage_dict)
+        
+        response = make_response(
+            jsonify(garages),
+            200
+        )
+        return response
 
 class GarageByID(Resource):
-    pass
+    def get(self, id):
+        garage = Garage.query.filter_by(id=id).first()
+
+        if garage:
+            garage_dict = {
+                "id": garage.id,
+                "name": garage.name,
+                "location": garage.location,
+                "contact_number": garage.contact_number,
+                "services": []
+            }
+
+            for service in garage.services:
+                service_dict = {
+                    "id": service.id,
+                    "name": service.name,
+                    "description": service.description,
+                    "price": service.price,
+                    "spare_parts": []
+                }
+
+                for spare_part in service.spare_parts:
+                    spare_part_dict = {
+                        "id": spare_part.id,
+                        "name": spare_part.name,
+                        "description": spare_part.description,
+                        "price": spare_part.price,
+                        "image": spare_part.image
+                    }
+
+                    service_dict["spare_parts"].append(spare_part_dict)
+
+                garage_dict["services"].append(service_dict)
+
+            response = make_response(
+                jsonify(garage_dict),
+                200
+            )
+        else:
+            response = make_response(
+                jsonify({"error": "Garage not found"}),
+                404
+            )
+
+        return response
 
 class ServiceResource(Resource):
-    pass
+    def get(self):
+        services = []
+
+        for service in Service.query.all():
+            service_dict = {
+                "id": service.id,
+                "name": service.name,
+                "description": service.description,
+                "price": service.price
+            }
+            services.append(service_dict)
+
+        response = make_response(
+            jsonify(services),
+            200
+        )
+
+        return response
+
 
 class SparePartResource(Resource):
-    pass
+    def get(self):
+        spare_parts = []
+        for spare_part in SparePart.query.all():
+            spare_part_dict = {
+                "id": spare_part.id,
+                "name": spare_part.name,
+                "description": spare_part.description,
+                "price": spare_part.price,
+                "image": spare_part.image,
+            }
+            spare_parts.append(spare_part_dict)
+
+        response = make_response(
+            jsonify(spare_parts),
+            200
+        )
+
+        return response
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')

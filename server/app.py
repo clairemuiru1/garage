a4faeca5bb7e4aee016cd3b4f74af606d6fece4c
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2024-01-25 11:36:30 +0300

    Added React

--- server/app.py
+++ server/app.py
@@ -10,6 +10,8 @@ from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 from models import db, Garage ,Service , SparePart
+from flask_cors import CORS 
+
 
 app = Flask(__name__)
 # CORS(app)

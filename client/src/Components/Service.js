2ba485e1bf6552084a3336e60197e494fbe2a8df
clairemuiru1 <muiruclaire@gmail.com>
2024-01-28 11:18:26 +0300

    "add oder service buttons"

--- client/src/Components/Service.js
+++ client/src/Components/Service.js
@@ -9,16 +9,25 @@ function Service() {
       .then(setServices);
   }, []); // No need to include setServices in the dependency array
 
+  const handleTowButtonClick = () => {
+    alert("Tow truck will arrive in 1 hour");
+  };
+
+  const handleMechanicButtonClick = () => {
+    alert("Mechanic arrives in 30 mins");
+  };
+
   return (
     <section>
       <h2>Services</h2>
       <ul>
         {services.map((serviceItem) => (
           <li key={serviceItem.id}>
-         
             <p>Name: {serviceItem.name}</p>
             <p>Description: {serviceItem.description}</p>
             <p>Price: {serviceItem.price}</p>
+            <button onClick={handleTowButtonClick}>Tow a Car</button>
+            <button onClick={handleMechanicButtonClick}>Dial a Mechanic</button>
           </li>
         ))}
       </ul>
@@ -27,6 +36,3 @@ function Service() {
 }
 
 export default Service;
-
-
-
